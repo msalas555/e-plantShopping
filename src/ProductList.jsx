700aed8e8f86d94427be6ccfddef91c3e65b66a0
msalas555 <salasmichael2004@gmail.com>
2024-08-25 16:55:57 -0400

    final

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,6 +268,8 @@ const totalProduct = () => {
     return totalPlants;
     };
 
+const totalPlants = totalProduct();
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -285,7 +287,7 @@ const totalProduct = () => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><span className="cart_quantity_count">{totalProduct}</span></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><span className="cart_quantity_count">{totalPlants}</span></h1></a></div>
             </div>
         </div>
         {!showCart? (
@@ -302,7 +304,18 @@ const totalProduct = () => {
                         <div className='product-price'>{plant.cost}</div>
                         <p>{plant.description}</p>
                         {/*Similarly like the above plant.name show other details like description and cost*/}
-                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        <button  
+                            className= {cartItems.items.filter(
+                                (prod) => prod.name == plant.name
+                              ).length == 0
+                                ? "product-button"
+                                : "product-button added-to-cart"
+                            } onClick={() => handleAddToCart(plant)}>
+                              {cartItems.items.filter((prod) => prod.name == plant.name)
+                                .length == 0
+                                ? "Add to Cart"
+                                : "Added to Cart"}{" "}  
+                            </button>
                     </div>
             ))}
         </div>
